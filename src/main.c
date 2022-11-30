d83fc3a6d5f0f5554004bf1f63fbc8ed8a356329
arnaudderison <a.derison@thedotkom.com>
2022-11-30 21:35:01 +0100

    pipe methode

--- src/main.c
+++ src/main.c
@@ -9,7 +9,8 @@
 
 struct Command command;
 int main (){
-    char inputCommand[80];
+    char inputCommand[MAX_BUFFER];
+    int nbI = 0;
     
     while(1){
         printf("%s%sa.derison~minishell%s$", BOLD,COLOR_BLU, COLOR_NRM);
@@ -19,15 +20,23 @@ int main (){
         parseCommand(inputCommand, &command);
 
         if(isInternalCommand() == 0){
-            executeNormalCommande();
+            nbI = isPipeCommand();
+            if(nbI > 0){
+                printf("PIPE COMMAND\n");
+                executePipeCommand(nbI);
+            }else{
+                executeNormalCommande();
+            }
         }
+        
 
         if((strcmp("exit", inputCommand) == 0) || (strcmp("exit\n", inputCommand) == 0)){
             printf("Bye\n");
             break;
         }
+
+        clearArg();
     }
-    //free(inputCommand);
     
     return 0;
 }
@@ -35,13 +44,16 @@ int main (){
 int parseCommand(char *inputCommand, struct Command *command){
     int i = 0;
     char *token;
-    token = strtok(inputCommand, " ");
+    
+    token = strtok(inputCommand, " \r\n");
 
-    while(token != NULL){
+    while(token != 0){
         command->argv[i] = token; 
-        token =  strtok(NULL, " ");    
+        token =  strtok(0, " \r\n");
         i++;
     }
+    
+    //command->argv[strcspn(token, "\n")] = 0;
     command->argc = i;
     command->commandName = command->argv[0];
 
@@ -53,7 +65,7 @@ int parseCommand(char *inputCommand, struct Command *command){
         printf("command->argv[%i] %s\n",j, command->argv[j]);
     }
     */
-    
+     
 }
 
 int readCommand(char *inputCommand){
@@ -64,7 +76,7 @@ int readCommand(char *inputCommand){
         currentChar = getchar();
         inputCommand[count++] = currentChar;
     };
-    inputCommand[count] = '\0'; //pour etre certain d'être a la fin de la commande actuel
+    inputCommand[count++] = '\0'; //pour etre certain d'être a la fin de la commande actuel
     return 0;
 }
 
@@ -102,7 +114,8 @@ int executeNormalCommande(){
         execvp(command.commandName, command.argv); // replace this process with commandName
 
         //not execute if not err
-        printf("Child process erreur...\n");
+        //printf("Child process erreur...\n");
+        perror(command.commandName);
         return 0;
     }
     else if(pid > 0){
@@ -114,4 +127,92 @@ int executeNormalCommande(){
     }
 
     return 0;
+}
+
+void clearArg(){
+    for(int i = 0; i<command.argc; i++){
+        command.argv[i] = 0;
+    }
+}
+
+int isPipeCommand(){
+    int i = 0;
+    for(i; i<command.argc; i++){
+        if(strcmp("|", command.argv[i]) == 0){
+            return i;
+        }
+    }
+    return 0;
+}
+
+int executePipeCommand(int i){
+    char *NamecommandA, *NamecommandB;
+    char *argvA[5], *argvB[5];
+    int j,jj=0, y;
+    int fd[2];
+    int pid;
+
+    for(j = 0; j<i; j++){
+        argvA[j] = command.argv[j];
+    }
+    argvA[j] = 0;
+    NamecommandA = argvA[0];
+    i++;
+
+    for(y = i; y<command.argc; y++){
+        argvB[jj] = command.argv[y];
+        jj++;
+    }
+    argvB[j] = 0;
+    NamecommandB = argvB[0];
+
+    //gest process
+
+     if(pipe(fd)){
+        perror("Pipe Error...");
+        exit(127);
+     }
+    
+    switch (pid = fork())
+    {
+    case -1:
+        perror("Fork Error...");
+        return 0;
+    case 0:
+        /*Child*/
+        close(fd[0]);
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[1]);
+        //char *argA[] = {"cat", "main.c", (char*)0};
+        //char *cmd = "cat";
+        execvp(NamecommandA, argvA);
+        perror(NamecommandA);
+        exit(126);
+        return 0;
+    
+    default:
+        /*Parent*/
+        waitpid(pid, NULL, 0);
+        close(fd[1]);
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[0]);
+        //char *argB[] = {"grep", "main", (char*)0};
+        //char *cmdB = "grep";
+        execvp(NamecommandB, argvB);
+        perror(NamecommandB);
+        exit(125);
+        return 0;
+    }
+
+
+
+    /*--------FOR DEBUG--------*/
+    
+    printf("%d\n", i);
+    printf("%d\n", command.argc);
+    for(int j = 0; j < 5; j++){
+        printf("argvA[%i] = %s ", j, argvA[j]);
+        printf("argvB[%i] = %s\n", j, argvB[j]);
+    }
+    
 }
